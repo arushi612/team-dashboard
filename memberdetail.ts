8ce0c49632c2b2a31e8ec28346e6fd76599ac777
arushi612 <an.ashi.clicks@gmail.com>
2017-06-12 11:02:57 +0530

    Add files via upload
    Ryt aligned - by sir
    ---------------------------
    index.html --> In src
    Rest --> In app folder

--- memberdetail.ts
+++ memberdetail.ts
@@ -14,9 +14,6 @@ export class MemberDetail
         this.name=name;
         this.email=email;
         this.id=id;
-       //this.main.push( new MemberDetail(name , email , id) ) 
-
-       //console.log(this.main[0])
     }
 }
 
@@ -29,20 +26,12 @@ export class TeamDetail
     id:number;
    main:MemberDetail[];
 
-
-
     constructor(tid,tname,name,email , main )
     {
         this.Tid=tid;
         this.Tname=tname;
         this.name=name;
         this.email=email;
-        this.id= tid;
-
         this.main = main
-     
     }
-
-
-
 }
